f43ae3bfbdbfe05d86029b344e1f9523a317c88f
User-2022HPETGW\Administrator <qy15@naver.com>
2023-05-30 17:13:29 +0900

    0530

---
+++ 38.js-제이쿼리/js/study2.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  //문서가 쭉 읽힌 뒤 실행될 소스코드
+  $("p").css("color","green")
+  $("p").css("font-weight","bold")
+  $("p").css("color","navy").css("font-weight","normal")
+  //체이닝기법으로도 메서드를 활용할 수 있다.
+
+$(".naverLink").attr("href","http://www.naver.com")
+let link = $(".naverLink").attr("href")
+console.log(link)
+
+$(".textframe>p:nth-of-type(1)").html("<i>text()함수</i>는 선택한 태그의 안에 들어가는 내용을 새로운 내용으로 교체할 수 있다.")
+
+let textP = $(".textframe>p:nth-of-type(2)").text()
+console.log(textP)
+
+$("#a").addClass("on")
+document.querySelector("#a")?.classList.add("on")
+$("#a").removeClass("on")
+document.querySelector("#a")?.classList.remove("on")
+
+
+$(".list>li").css("border-bottom","1px solid black")
+
+$(".list").children().css("color","blue")
+// 위 한 줄은 아래 자바스크립트 언어와 같다.
+// let listArray = document.querySelector(".list")?.children
+// for(let i=0;i<listArray?.length;i++){
+//   listArray[i].style.color="red"
+// }
+
+$(".list>li").parent().css("background","skyblue")
+$(".list>li:nth-of-type(2)").next().css("text-align","center")
+$(".list>li:nth-of-type(2)").prev().css("text-align","right")
+
+$(".listNum>li").eq(0).css("color","red")
+
+
+
+
+
+})
+
+
--- pearlabyss-ex/js/common.js
+++ pearlabyss-ex/js/common.js
@@ -18,4 +18,32 @@ window.onload = function () {
 
   });
 
+  let noticeSlider = new Swiper(".notice", {
+    
+    pagination: {
+      el: '.swiper-pagination2',
+      // type: 'bullets',
+    },
+    
+    breakpoints: {
+      280: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 4
+      }
+    }
+
+  });
+
 }
+
+// function naraSwiper(selector,option){
+//   this.lists = document.querySelector(selector).children[0].children.length
+//   document.querySelector(selector).children[0].style.width = this.lists*100/option.perview+"%"
+//   document.querySelector(selector).children[0].display = "flex"
+
+// }
+// naraSwiper(".notice",{
+//   perview:2
+// })
