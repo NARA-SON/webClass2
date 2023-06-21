14feb23b9766ef6ab17c3b60d081aaec31901891
User-2022HPETGW\Administrator <qy15@naver.com>
2023-06-21 18:10:52 +0900

    0621

--- clinique/js/moistSlider.js
+++ clinique/js/moistSlider.js
@@ -1,20 +1,44 @@
 $(document).ready(function () {
 
-  let count = 2;
+  let count = 0;
 
-  $(".msBtn").click(function () {
+  $(".msImgTrain > li").eq(0).clone().appendTo(".msImgTrain");
+  $(".msImgTrain > li").eq(1).clone().appendTo(".msImgTrain");
+  $(".msImgTrain > li").eq(2).clone().appendTo(".msImgTrain");
+  // slideMove(1);
 
-    count++
-    if (count > 11) { count = 0 }
-    $(".msImgTrain").css
-      ("transform", "translateX(" + (-8.333 * count) + "%)")
+  $(".msBtn").click(function(){
+    count++;
+    slideMove(count);
+    if (count == 8) { 
+      count = 0
+      setTimeout(function () {
+        turnOffTransition()
+        slideMove(0)
+      }, 700)
+
+      setTimeout(function () {
+        turnOnTransition()
+      },800)
+      
+    }
+    
+    console.log(count)
 
     $(".msTxTrain li").removeClass("on")
     $(".msTxTrain li").eq(count).addClass("on")
 
-  })
+  });
 
-  $(".msImgTrain>li").eq(9).css("transition","none")
+  function slideMove(slideCount){
+    $(".msImgTrain").css("transform","translateX("+(-(100/11))*slideCount+"%)");
+  }
+  function turnOffTransition(){
+    $(".msImgTrain").css("transition","all 0s ease 0s");
+  }
+  function turnOnTransition(){
+    $(".msImgTrain").css("transition","all 0.7s ease 0s");
+  }
 
 
   })
