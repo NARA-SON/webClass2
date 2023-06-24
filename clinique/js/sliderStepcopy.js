3c81d44dc2c194442ab5dc603c6a1314a7f3d0d6
Nara Son <agnes1890@naver.com>
2023-06-24 18:03:19 +0900

    0624

--- clinique/js/sliderStepcopy.js
+++ clinique/js/sliderStepcopy.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
   $(".sp1ImgTrain > li").eq(0).clone().appendTo(".sp1ImgTrain");
   $(".sp1ImgTrain > li").eq(1).clone().appendTo(".sp1ImgTrain");
   $(".sp1ImgTrain > li").eq(2).clone().appendTo(".sp1ImgTrain");
+
   // slideMove(0);
 
   // 클릭이벤트 : 슬라이드가 아래와같이 작동
@@ -12,29 +13,29 @@ $(document).ready(function () {
     count++;
     slideMove(count);
     if (count == 4) { 
-      count = 0
+      count=0; 
       //0.7초 후에 이동을 멈추고 0번째로 이동
       setTimeout(function () {
         turnOffTransition()
         slideMove(0)
       }, 700)
-
+      
       //0.8초 후에 이동시작
       setTimeout(function () {
         turnOnTransition()
-      },800)
-      
-    }
+      },800)    
+  }
 
     console.log(count)
     
     //클릭 후 on클래스가 붙은 이미지만 오른쪽 텍스트칸 400px 비우기, 투명도 정상으로 변경
-    $(".sp1ImgTrain li").removeClass("on")
-    $(".sp1ImgTrain li").eq(count).addClass("on")
+    // $(".sp1ImgTrain li").removeClass("on")
+    // $(".sp1ImgTrain li").eq(count).addClass("on")
 
     // 클릭 후 on클래스가 붙은 텍스트만 보인다.
     $(".sp1TxTrain li").removeClass("on")
     $(".sp1TxTrain li").eq(count).addClass("on")
+
   });//클릭이벤트종료
 
   //함수 : 기차칸 이동
